28d16294a096c79df49ad2998122d2ae4db487bf
Minamoto Xu <nicai@unknow.com>
2019-05-17 15:30:41 +0800

    upgrade rmq

--- mq/rmq.go
+++ mq/rmq.go
@@ -2,342 +2,315 @@ package mq
 
 import (
 	"fmt"
-	"io/ioutil"
 	"strings"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 	"github.com/xyzj/gopsu"
 )
 
-// RMQConsumer rabbit-mq Consumer struct
-type RMQConsumer struct {
-	logRmq      *gopsu.MxLog        // 日志
-	verbose     bool                // 是否打印信息
-	chanRecv    chan *amqp.Delivery // 接收队列
-	chanClose   chan bool           // 子线程close
-	chanWatcher chan string         // 子线程监视通道
+const (
+	queueMaxLength = 70000
+)
 
-	connStr      string   // 连接字符串
-	exchangeName string   // 交换机名称
-	exchangeType string   // 交换机类型
-	routingKeys  []string // 过滤器
-	queueName    string   // 队列名
-	queueDurable bool     // 队列是否持久化
-	queueDelete  bool     // 队列在不用时是否删除
-	queueMax     int32    // 队列长度
-	addr         string   // ip:port
+// RabbitMQData rabbit-mq data send struct
+type RabbitMQData struct {
+	RoutingKey string
+	Data       *amqp.Publishing
 }
 
-// RMQProducer 生产者
-type RMQProducer struct {
-	logRmq      *gopsu.MxLog       // 日志
-	verbose     bool               // 是否打印信息
-	chanSend    chan *RabbitMQData // send queue
-	chanClose   chan bool          // 子线程close
-	chanWatcher chan string        // 子线程监视通道
-
-	connStr      string // 连接字符串
-	exchangeName string // 交换机名称
-	exchangeType string // 交换机类型
-	addr         string // ip:port
+// Session rmq session
+type Session struct {
+	name          string
+	logger        *gopsu.MxLog
+	connection    *amqp.Connection
+	channel       *amqp.Channel
+	done          chan bool
+	closeMe       bool
+	isReady       bool
+	debug         bool
+	connStr       string
+	addr          string
+	queue         amqp.Queue
+	routingKeys   []string           // 过滤器
+	queueName     string             // 队列名
+	queueDurable  bool               // 队列是否持久化
+	queueDelete   bool               // 队列在不用时是否删除
+	queueDelivery chan amqp.Delivery // 消息
 }
 
-// NewConsumer 新的消费者
-func NewConsumer(conn, exchangeName, exchangeType, queueName string, routingKeys []string) *RMQConsumer {
-	return &RMQConsumer{
-		connStr:      conn,
-		exchangeName: exchangeName,
-		exchangeType: exchangeType,
-		queueName:    queueName,
-		routingKeys:  routingKeys,
-		queueDurable: true,
-		queueMax:     70000,
-		chanRecv:     make(chan *amqp.Delivery, 2000),
-		chanClose:    make(chan bool, 2),
-		chanWatcher:  make(chan string, 2),
-		addr:         strings.Split(conn, "@")[1],
+// NewConsumer 初始化消费者实例
+func NewConsumer(name, connstr, queuename string, logger *gopsu.MxLog, durable, autodel, debug bool) *Session {
+	sessn := &Session{
+		logger:        logger,
+		name:          name,
+		connStr:       connstr,
+		debug:         debug,
+		done:          make(chan bool),
+		queueName:     queuename,
+		queueDurable:  durable,
+		queueDelete:   autodel,
+		queueDelivery: make(chan amqp.Delivery),
+		closeMe:       false,
+	}
+	sessn.addr = strings.Split(connstr, "@")[1]
+	go sessn.handleReconnect("consumer")
+	return sessn
+}
+
+// NewProducer 初始化生产者实例
+func NewProducer(name, connstr string, logger *gopsu.MxLog, debug bool) *Session {
+	sessn := &Session{
+		logger:  logger,
+		name:    name,
+		connStr: connstr,
+		debug:   debug,
+		done:    make(chan bool),
+	}
+	sessn.addr = strings.Split(connstr, "@")[1]
+	go sessn.handleReconnect("producer")
+	return sessn
+}
+
+func (sessn *Session) logInfo(s string) {
+	if sessn.logger != nil {
+		sessn.logger.Info(s)
+	} else {
+		if sessn.debug {
+			fmt.Printf("%s [debug] %s\n", time.Now().Format("01-02 15:04:05"), s)
+		}
+	}
+}
+func (sessn *Session) logWarning(s string) {
+	if sessn.logger != nil {
+		sessn.logger.Warning(s)
+	} else {
+		if sessn.debug {
+			fmt.Printf("%s [debug] %s\n", time.Now().Format("01-02 15:04:05"), s)
+		}
+	}
+}
+func (sessn *Session) logError(s string) {
+	if sessn.logger != nil {
+		sessn.logger.Error(s)
+	} else {
+		if sessn.debug {
+			fmt.Printf("%s [debug] %s\n", time.Now().Format("01-02 15:04:05"), s)
+		}
+	}
+}
+func (sessn *Session) logSystem(s string) {
+	if sessn.logger != nil {
+		sessn.logger.System(s)
+	} else {
+		if sessn.debug {
+			fmt.Printf("%s [debug] %s\n", time.Now().Format("01-02 15:04:05"), s)
+		}
 	}
 }
 
-// SetLogger 设置日志
-func (r *RMQConsumer) SetLogger(l *gopsu.MxLog) {
-	r.logRmq = l
-}
-
-// SetDebug 设置是否调试
-func (r *RMQConsumer) SetDebug(b bool) {
-	r.verbose = b
-}
-
-// Recv 接收数据
-func (r *RMQConsumer) Recv() *amqp.Delivery {
-	return <-r.chanRecv
-}
-
-// Start Start consumer
-func (r *RMQConsumer) Start() {
-	go r.coreWatcher()
-	r.chanWatcher <- "consumer"
-}
-
-// Close 关闭消费者
-func (r *RMQConsumer) Close() {
-	r.chanClose <- true
-}
-
-// String 返回ip:port
-func (r *RMQConsumer) String() string {
-	return r.addr
-}
-
-func (r *RMQConsumer) coreWatcher() {
-	defer func() {
-		if err := recover(); err != nil {
-			ioutil.WriteFile(fmt.Sprintf("crash-rmq-%s.log", time.Now().Format("20060102150405")), []byte(fmt.Sprintf("%v", errors.WithStack(err.(error)))), 0644)
-			time.Sleep(300 * time.Millisecond)
+// handleReconnect 维护连接
+func (sessn *Session) handleReconnect(t string) {
+	if sessn.connect() {
+		switch t {
+		case "consumer":
+			sessn.initConsumer()
+		case "producer":
+			sessn.initProducer()
 		}
-	}()
-	var closeme = false
+	}
 	for {
-		if closeme == true {
+		if sessn.closeMe {
 			break
 		}
 		select {
-		case n := <-r.chanWatcher:
-			time.Sleep(100 * time.Millisecond)
-			switch n {
-			case "consumer":
-				for {
-					conn, err := r.initConsumer()
-					if err != nil {
-						time.Sleep(15 * time.Second)
-					} else {
-						go r.handleConsumer(conn)
-						break
-					}
+		case <-sessn.done:
+			sessn.closeMe = true
+			sessn.channel.Close()
+			sessn.connection.Close()
+			_, ok := <-sessn.queueDelivery
+			if ok {
+			}
+		case <-time.After(10 * time.Second):
+			if sessn.isReady {
+				continue
+			}
+			if sessn.connect() {
+				switch t {
+				case "consumer":
+					sessn.initConsumer()
+				case "producer":
+					sessn.initProducer()
 				}
-				closeme = false
-			case "closeconsumer":
-				closeme = true
 			}
 		}
 	}
 }
 
-func (r *RMQConsumer) showMessages(s string, level int) {
-	if r.logRmq != nil {
-		switch level {
-		case 10:
-			r.logRmq.Debug(s)
-		case 20:
-			r.logRmq.Info(s)
-		case 30:
-			r.logRmq.Warning(s)
-		case 40:
-			r.logRmq.Error(s)
-		case 90:
-			r.logRmq.System(s)
-		}
-	}
-	if r.verbose {
-		println(s)
-	}
-}
+// connect 建立连接
+func (sessn *Session) connect() bool {
+	sessn.isReady = false
+	sessn.logInfo("Attempting to connect to " + sessn.addr)
+	conn, err := amqp.Dial(sessn.connStr)
 
-func (r *RMQConsumer) initConsumer() (*amqp.Connection, error) {
-	conn, err := amqp.Dial(r.connStr)
 	if err != nil {
-		return nil, err
+		sessn.logError("Failed to connnect to " + sessn.addr)
+		return false
 	}
-	return conn, nil
-}
-
-// 启动消费者线程
-func (r *RMQConsumer) handleConsumer(conn *amqp.Connection) {
-	defer func() {
-		if err := recover(); err != nil {
-			r.showMessages(fmt.Sprintf("RMQ Consumer goroutine crash: %s", err.(error).Error()), 40)
-			r.chanWatcher <- "consumer"
-		} else {
-			r.chanWatcher <- "closeconsumer"
-		}
-	}()
-	defer conn.Close()
+	sessn.connection = conn
 
 	ch, err := conn.Channel()
 	if err != nil {
-		panic(err)
+		sessn.logError("Failed to open channel: " + err.Error())
+		return false
 	}
-	defer ch.Close()
+	sessn.channel = ch
 
-	err = ch.ExchangeDeclare(
-		r.exchangeName, // name
-		r.exchangeType, // type
-		true,           // durable
-		false,          // auto-deleted
-		false,          // internal
-		false,          // no-wait
-		nil,            // arguments
+	err = sessn.channel.ExchangeDeclare(
+		sessn.name, // name
+		"topic",    // type
+		true,       // durable
+		true,       // auto-deleted
+		false,      // internal
+		false,      // no-wait
+		nil,        // arguments
 	)
 	if err != nil {
-		panic(err)
+		sessn.logError("Failed to declare exchange: " + err.Error())
+		return false
 	}
-	q, err := ch.QueueDeclare(
-		r.queueName,    // name
-		r.queueDurable, // durable
-		r.queueDelete,  // delete when unused
-		false,          // exclusive
-		false,          // no-wait
+
+	sessn.logInfo("Success to connect to " + sessn.addr)
+
+	sessn.isReady = true
+	return true
+}
+
+// IsReady 是否就绪
+func (sessn *Session) IsReady() bool {
+	return sessn.isReady
+}
+
+// WaitReady 等待就绪，0-默认超时5s
+func (sessn *Session) WaitReady(second int) bool {
+	if second == 0 {
+		second = 5
+	}
+	for {
+		select {
+		case <-time.After(time.Millisecond * 10):
+			if sessn.isReady {
+				return true
+			}
+		case <-time.Tick(time.Duration(second) * time.Second):
+			if sessn.isReady {
+				return true
+			}
+			return false
+		}
+	}
+}
+
+// Close 关闭
+func (sessn *Session) Close() {
+	sessn.done <- true
+}
+
+func (sessn *Session) initConsumer() {
+	// defer func() {
+	// 	if err := recover(); err != nil {
+	// 		sessn.isReady = false
+	// 		sessn.logError("Consumer core error: " + errors.WithStack(err.(error)).Error())
+	// 	}
+	// }()
+	var err error
+	_, err = sessn.channel.QueueDeclare(
+		sessn.queueName,    // name
+		sessn.queueDurable, // durable
+		sessn.queueDelete,  // delete when unused
+		false,              // exclusive
+		false,              // no-wait
 		amqp.Table{
-			"x-max-length": r.queueMax,
+			"x-max-length": queueMaxLength,
 		}, // arguments
 	)
 	if err != nil {
-		ch.QueueDelete(r.exchangeName, false, false, true)
-		panic(err)
+		sessn.logError("Failed to create queue " + sessn.queueName + ": " + err.Error())
+		sessn.isReady = false
+		return
 	}
-	for _, v := range r.routingKeys {
-		err = ch.QueueBind(q.Name, // queue name
-			v,              // routing key
-			r.exchangeName, // exchange
-			false,
-			nil)
-		if err != nil {
-			panic(err)
-		}
+
+	// delivery, err := sessn.channel.Consume(sessn.name, // queue
+	// 	"",    // consumer
+	// 	true,  // auto ack
+	// 	false, // exclusive
+	// 	false, // no local
+	// 	false, // no wait
+	// 	nil,   // args
+	// )
+	// if err != nil {
+	// 	sessn.logError("Failed to create consume " + sessn.queueName + ": " + err.Error())
+	// 	sessn.isReady = false
+	// 	return
+	// }
+
+	// for {
+	// 	if sessn.closeMe {
+	// 		return
+	// 	}
+	// 	select {
+	// 	case msg := <-delivery:
+	// 		sessn.queueDelivery <- msg
+	// 	}
+	// }
+}
+
+// Recv 接收消息
+func (sessn *Session) Recv() (<-chan amqp.Delivery, error) {
+	if !sessn.isReady {
+		return nil, fmt.Errorf("no connected")
 	}
-	chanMsgs, err := ch.Consume(q.Name, // queue
-		"",    // consumer
-		true,  // auto ack
-		false, // exclusive
-		false, // no local
-		false, // no wait
-		nil,   // args
+	// d, ok, err := sessn.channel.Get(sessn.queueName, true)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// if !ok {
+	// 	return nil, fmt.Errorf("no message")
+	// }
+	// return &d, nil
+	return sessn.channel.Consume(
+		sessn.queueName,
+		"",    // Consumer
+		true,  // Auto-Ack
+		false, // Exclusive
+		false, // No-local
+		false, // No-Wait
+		nil,   // Args
 	)
-	if err != nil {
-		panic(err)
+}
+
+// BindKey 绑定过滤器
+func (sessn *Session) BindKey(k string) error {
+	if sessn.isReady {
+		return sessn.channel.QueueBind(sessn.queueName, k, sessn.name, false, nil)
 	}
-	r.showMessages(fmt.Sprintf("%s RMQ Consumer connect to Rabbit-MQ Server.", gopsu.Stamp2Time(time.Now().Unix())[:10]), 90)
-	closeme := false
-	for {
-		if closeme {
-			break
-		}
-		select {
-		case msg := <-chanMsgs:
-			r.chanRecv <- &msg
-		case <-r.chanClose:
-			closeme = true
-		}
+	return fmt.Errorf("Failed to bind key, channel not ready")
+}
+
+// UnBindKey 解绑过滤器
+func (sessn *Session) UnBindKey(k string) error {
+	if sessn.isReady {
+		return sessn.channel.QueueUnbind(sessn.queueName, k, sessn.name, nil)
 	}
+	return fmt.Errorf("Failed to Unbind key, channel not ready")
 }
+func (sessn *Session) initProducer() {
 
-// NewProducer 新的生产
-func NewProducer(conn, exchangeName, exchangeType string) *RMQProducer {
-	return &RMQProducer{
-		connStr:      conn,
-		exchangeName: exchangeName,
-		exchangeType: exchangeType,
-		chanSend:     make(chan *RabbitMQData, 5000),
-		chanClose:    make(chan bool, 2),
-		chanWatcher:  make(chan string, 2),
-		addr:         strings.Split(conn, "@")[1],
-	}
-}
-
-// SetLogger 设置日志
-func (r *RMQProducer) SetLogger(l *gopsu.MxLog) {
-	r.logRmq = l
-}
-
-// SetDebug 设置是否调试
-func (r *RMQProducer) SetDebug(b bool) {
-	r.verbose = b
-}
-
-// Start Start producer
-func (r *RMQProducer) Start() {
-	go r.coreWatcher()
-	r.chanWatcher <- "producer"
-}
-
-// Close 关闭消费者
-func (r *RMQProducer) Close() {
-	r.chanClose <- true
-}
-
-// String 返回ip:port
-func (r *RMQProducer) String() string {
-	return r.addr
-}
-
-func (r *RMQProducer) coreWatcher() {
-	defer func() {
-		if err := recover(); err != nil {
-			ioutil.WriteFile(fmt.Sprintf("crash-rmq-%s.log", time.Now().Format("20060102150405")), []byte(fmt.Sprintf("%v", errors.WithStack(err.(error)))), 0644)
-			time.Sleep(300 * time.Millisecond)
-		}
-	}()
-	var closeme = false
-	for {
-		if closeme == true {
-			break
-		}
-		select {
-		case n := <-r.chanWatcher:
-			time.Sleep(100 * time.Millisecond)
-			switch n {
-			case "producer":
-				for {
-					conn, err := r.initProducer()
-					if err != nil {
-						time.Sleep(15 * time.Second)
-					} else {
-						go r.handleProducer(conn)
-						break
-					}
-				}
-				closeme = false
-			case "closeproducer":
-				closeme = true
-			}
-		}
-	}
-}
-
-func (r *RMQProducer) showMessages(s string, level int) {
-	if r.logRmq != nil {
-		switch level {
-		case 10:
-			r.logRmq.Debug(s)
-		case 20:
-			r.logRmq.Info(s)
-		case 30:
-			r.logRmq.Warning(s)
-		case 40:
-			r.logRmq.Error(s)
-		case 90:
-			r.logRmq.System(s)
-		}
-	}
-	if r.verbose {
-		println(s)
-	}
-}
-
-func (r *RMQProducer) initProducer() (*amqp.Connection, error) {
-	conn, err := amqp.Dial(r.connStr)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
 }
 
 // Send 发送数据,默认数据有效期10分钟
-func (r *RMQProducer) Send(f string, d []byte) {
-	r.SendCustom(&RabbitMQData{
+func (sessn *Session) Send(f string, d []byte) {
+	sessn.SendCustom(&RabbitMQData{
 		RoutingKey: f,
 		Data: &amqp.Publishing{
 			ContentType:  "text/plain",
@@ -358,74 +331,28 @@ func (r *RMQProducer) Send(f string, d []byte) {
 // 	Timestamp:    time.Now(),
 // 	Body:         []byte("abcd"),
 // },
-func (r *RMQProducer) SendCustom(d *RabbitMQData) {
-	if r.chanSend == nil {
+func (sessn *Session) SendCustom(d *RabbitMQData) {
+	if !sessn.isReady {
 		return
 	}
 	go func() {
 		defer func() { recover() }()
-		r.chanSend <- d
-	}()
-}
-
-// 启动生产者线程
-func (r *RMQProducer) handleProducer(conn *amqp.Connection) {
-	defer func() {
-		if err := recover(); err != nil {
-			r.showMessages(fmt.Sprintf("RMQ Producer goroutine crash: %s", err.(error).Error()), 40)
-			r.chanWatcher <- "producer"
-		} else {
-			r.chanWatcher <- "closeproducer"
+		err := sessn.channel.Publish(
+			sessn.name,   // exchange
+			d.RoutingKey, // routing key
+			false,        // mandatory
+			false,        // immediate
+			*d.Data,
+			// amqp.Publishing{
+			// 	ContentType:  "text/plain",
+			// 	DeliveryMode: amqp.Persistent,
+			// 	Expiration:   "300000",
+			// 	Timestamp:    time.Now(),
+			// 	Body:         []byte(msg[1]),
+			// }
+		)
+		if err != nil {
+			sessn.logError("Failed to send to " + sessn.addr + ": " + err.Error())
 		}
 	}()
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		panic(err)
-	}
-	defer ch.Close()
-
-	err = ch.ExchangeDeclare(
-		r.exchangeName, // name
-		r.exchangeType, // type
-		true,           // durable
-		false,          // auto-deleted
-		false,          // internal
-		false,          // no-wait
-		nil,            // arguments
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	r.showMessages(fmt.Sprintf("%s RMQ Producer connect to Rabbit-MQ Server.", gopsu.Stamp2Time(time.Now().Unix())[:10]), 90)
-	closeme := false
-	for {
-		if closeme {
-			break
-		}
-		select {
-		case msg := <-r.chanSend:
-			err = ch.Publish(
-				r.exchangeName, // exchange
-				msg.RoutingKey, // routing key
-				false,          // mandatory
-				false,          // immediate
-				*msg.Data,
-				// amqp.Publishing{
-				// 	ContentType:  "text/plain",
-				// 	DeliveryMode: amqp.Persistent,
-				// 	Expiration:   "300000",
-				// 	Timestamp:    time.Now(),
-				// 	Body:         []byte(msg[1]),
-				// }
-			)
-			if err != nil {
-				panic(err)
-			}
-		case <-r.chanClose:
-			closeme = true
-		}
-	}
 }
