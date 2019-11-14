d750286ecb7de3838431cbe55850c6ce73e1f853
Minamoto Xu <minamoto.xu@outlook.com>
2019-11-14 22:34:42 +0800

    bug fix

--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	sql := db.SQLPool{
-		Server:      "180.153.108.83:3306",
+		Server:      "180.167.245.233:3306",
 		User:        "root",
 		Passwd:      "lp1234xy",
 		CacheDir:    ".",
@@ -18,16 +18,16 @@ func main() {
 	}
 	err := sql.New()
 	if err != nil {
-		println(err.Error())
+		println("conn", err.Error())
 		return
 	}
 	strsql := "select * from user_list"
 	s, err := sql.QueryJSON(strsql, 0)
 	if err != nil {
-		println(err.Error())
+		println("query", err.Error())
 		return
 	}
 	println(s)
-	s, _ = sql.QueryJSON("select * from user_rwx", 0)
-	println(s)
+	// s, _ = sql.QueryJSON("select * from user_rwx", 0)
+	// println(s)
 }
