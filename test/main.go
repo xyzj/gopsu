2bf14aa7287860ecc4bd5a4714ee094d76756e16
Minamoto Xu <minamoto.xu@outlook.com>
2019-11-16 01:00:56 +0800

    优化性能

--- test/main.go
+++ test/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
-	"github.com/tidwall/gjson"
 	"github.com/xyzj/gopsu"
 	db "github.com/xyzj/gopsu/db"
 )
@@ -16,25 +16,37 @@ func main() {
 		CacheDir:    ".",
 		EnableCache: true,
 		DriverType:  db.DriverMYSQL,
-		DataBase:    "mydb10001_data",
+		DataBase:    "mydb10001",
 		Logger:      &gopsu.StdLogger{},
-		Timeout:     100,
+		Timeout:     10,
 	}
 	err := sql.New()
 	if err != nil {
 		println("conn", err.Error())
 		return
 	}
-	t1 := time.Now().Unix()
-	strsql := "select * from data_rtu_record"
-	s, err := sql.QueryJSON(strsql, 0)
-	if err != nil {
-		println("query", err.Error())
-		return
+	strsql := "select * from user_rwx"
+	strsql = "truncate table user_rwx"
+	sql.Exec(strsql)
+	strsql = "insert into user_rwx set user_name=?,r=?,w=?,x=?,d=?"
+	params := make([]interface{}, 5000)
+	for i := 0; i < 5000; i += 5 {
+		params[i] = fmt.Sprintf("user%d", i)
+		params[i+1] = 0
+		params[i+2] = 0
+		params[i+3] = 0
+		params[i+4] = 0
 	}
-	t2 := time.Now().Unix()
-
-	println(t2-t1, gjson.Parse(s).Get("total").Int())
+	t1 := time.Now()
+	err = sql.ExecPrepare(strsql, 0, params...)
+	if err != nil {
+		println(err.Error())
+	}
+	t2 := time.Now()
+	strsql = "select count(*) from user_rwx"
+	ans, _ := sql.QueryOne(strsql, 1)
+	println(ans)
+	println(t2.Sub(t1).Nanoseconds() / 1000000)
 	// s, _ = sql.QueryJSON("select * from user_rwx", 0)
 	// println(s)
 }
