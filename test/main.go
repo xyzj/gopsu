0fddb9ffd15b5abe647762e18e5d0f6dcd641e47
Minamoto Xu <nicai@unknow.com>
2019-11-15 10:13:05 +0800

    优化sql库，使用context控制操作超时

--- test/main.go
+++ test/main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
+	"time"
+
+	"github.com/tidwall/gjson"
 	"github.com/xyzj/gopsu"
-	db "github.com/xyzj/gopsu/sql"
+	db "github.com/xyzj/gopsu/db"
 )
 
 func main() {
 	sql := db.SQLPool{
-		Server:      "180.167.245.233:3306",
+		Server:      "180.153.108.83:3306",
 		User:        "root",
 		Passwd:      "lp1234xy",
 		CacheDir:    ".",
 		EnableCache: true,
 		DriverType:  db.DriverMYSQL,
-		DataBase:    "mydb10001",
+		DataBase:    "mydb10001_data",
 		Logger:      &gopsu.StdLogger{},
+		Timeout:     50,
 	}
 	err := sql.New()
 	if err != nil {
 		println("conn", err.Error())
 		return
 	}
-	strsql := "select * from user_list"
+	t1 := time.Now().Unix()
+	strsql := "select * from data_rtu_loop_record"
 	s, err := sql.QueryJSON(strsql, 0)
 	if err != nil {
 		println("query", err.Error())
 		return
 	}
-	println(s)
+	t2 := time.Now().Unix()
+
+	println(t2-t1, gjson.Parse(s).Get("total").Int())
 	// s, _ = sql.QueryJSON("select * from user_rwx", 0)
 	// println(s)
 }
