791179196a332b2ad70dbe256f06540c0dc3e355
Minamoto Xu <nicai@unknow.com>
2019-12-06 14:00:31 +0800

    fix sql isready

--- test/main.go
+++ test/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"github.com/xyzj/gopsu/test/lib"
-	"gitlab.local/wlstmicro"
+	"github.com/xyzj/gopsu"
 )
 
 // 启动文件 main.go
 func main() {
-	// 启动生产者，用于发送消息
-	wlstmicro.StartMQConsumer()
-
-	// 启动消费者，用于接收消息
-	lib.NewMQConsumer()
+	println(gopsu.GetUUID1())
 }
