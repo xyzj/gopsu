f010e82370588797278bc98e58e75ed644fc44f7
Minamoto Xu <nicai@unknow.com>
2019-12-25 16:38:46 +0800

    fix rmq send log

--- mq/rmq.go
+++ mq/rmq.go
@@ -357,6 +357,7 @@ func (sessn *Session) SendCustom(d *RabbitMQData) {
 		)
 		if err != nil {
 			sessn.logger.Error("SndErr:" + sessn.addr + "|" + err.Error() + "|" + d.RoutingKey)
+			return
 		}
 		sessn.logger.Info("S:" + sessn.addr + "|" + d.RoutingKey + "|" + FormatMQBody(d.Data.Body))
 	}()
