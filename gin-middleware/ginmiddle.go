1e7d75ca54d1d6c0893745a7fa873898e5e00fef
xy <xy@luningdeMacBook.local>
2020-03-11 00:24:23 -0700

    remove xfile

--- gin-middleware/ginmiddle.go
+++ gin-middleware/ginmiddle.go
@@ -197,7 +197,6 @@ func CheckRequired(params ...string) gin.HandlerFunc {
 			if c.Param(v) == "" {
 				c.Set("status", 0)
 				c.Set("detail", v)
-				c.Set("xfile", 5)
 				c.AbortWithStatusJSON(200, c.Keys)
 				break
 			}
