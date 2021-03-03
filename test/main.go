40b84aa5f7dadccbdadebada2db39c9f0c8e9214
Minamoto Xu <nicai@unknow.com>
2021-03-03 11:16:09 +0800

    增加返回runtime模板

--- test/main.go
+++ test/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/xyzj/gopsu"
-)
-
 var (
 	h   = -0.833
 	uto = 180
@@ -13,9 +7,4 @@ var (
 
 // 启动文件 main.go
 func main() {
-	a := float64(331907000000)
-	b := gopsu.Float642BcdBytesBigOrder(a, "%12.0f")
-	println(gopsu.Bytes2String(b, "-"))
-	c := gopsu.BcdBytes2Float64BigOrder(b, 0, true)
-	println(fmt.Sprintf("%12.0f,%12.0f", a, c))
 }
