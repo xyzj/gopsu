df3b17f5cf8c49f01699040be01175655da32fae
Minamoto Xu <nicai@unknow.com>
2021-03-03 16:13:42 +0800

    优化500页面

--- test/main.go
+++ test/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	ginmiddleware "github.com/xyzj/gopsu/gin-middleware"
+)
+
 var (
 	h   = -0.833
 	uto = 180
@@ -7,4 +11,8 @@ var (
 
 // 启动文件 main.go
 func main() {
+	r := ginmiddleware.NewGinEngine("log", "test.log", 1)
+	r.Static("/ttt", "log")
+	r.GET("/500", ginmiddleware.Page403)
+	r.Run(":8080")
 }
