ea4c4f3d2bfe0788242797b188a9107e97166d4f
unknow <beunknow@outlook.com>
2022-06-16 13:36:24 +0800

    add go.mod

--- loopfunc/loop.go
+++ loopfunc/loop.go
@@ -8,16 +8,23 @@ import (
 // LoopFunc 执行循环工作，并提供panic恢复
 func LoopFunc(f func(params ...interface{}), params ...interface{}) {
 	locker := &sync.WaitGroup{}
+	end := false
 RUN:
 	locker.Add(1)
 	go func() {
 		defer func() {
-			recover()
+			if err := recover(); err == nil {
+				// 非panic,不需要恢复
+				end = true
+			}
 			locker.Done()
 		}()
 		f(params...)
 	}()
 	locker.Wait()
+	if end {
+		return
+	}
 	time.Sleep(time.Second * 3)
 	goto RUN
 }
