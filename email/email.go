c373816874854b0a0e37a1e5ee31412b0d5b211a
unknow <beunknow@outlook.com>
2022-07-19 16:30:17 +0800

    add email send support, office365 mail server is work

---
+++ email/email.go
@@ -0,0 +1,91 @@
+package email
+
+import (
+	"crypto/tls"
+	"fmt"
+	"sync"
+	"time"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+// SMTPOpt smtp config
+type SMTPOpt struct {
+	// Username login username
+	Username string
+	// Passwd login password
+	Passwd string
+	// SMTPHost smtp server host
+	SMTPHost string
+	// SMTPPort smtp server port, default: 587
+	SMTPPort int
+}
+
+// Data email body
+type Data struct {
+	// From sender name, if empty, use username
+	From string
+	// To target email address
+	To string
+	// Subject email title
+	Subject string
+	// Cc cc to others
+	Cc []string
+	// Msg email body, default send as html
+	Msg string
+}
+
+// EMail emal send client
+type EMail struct {
+	locker   *sync.Mutex
+	dialer   *gomail.Dialer
+	message  *gomail.Message
+	username string
+}
+
+// NewEMail get a new email send client
+func NewEMail(opt *SMTPOpt) (*EMail, error) {
+	if opt == nil {
+		return nil, fmt.Errorf("illegal smtp config")
+	}
+	if opt.SMTPHost == "" {
+		return nil, fmt.Errorf("illegal smtp config")
+	}
+	if opt.SMTPPort < 1 || opt.SMTPPort > 65535 {
+		opt.SMTPPort = 587
+	}
+	d := gomail.NewDialer(opt.SMTPHost, opt.SMTPPort, opt.Username, opt.Passwd)
+	if d != nil {
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+		return &EMail{
+			locker:   &sync.Mutex{},
+			dialer:   d,
+			username: opt.Username,
+			message:  gomail.NewMessage(),
+		}, nil
+	}
+
+	return nil, fmt.Errorf("unknow error")
+}
+
+// Send send email to target
+func (e *EMail) Send(d *Data) error {
+	if e == nil {
+		return fmt.Errorf("email client is not ready")
+	}
+	if d == nil {
+		return fmt.Errorf("nothing to send")
+	}
+	e.locker.Lock()
+	defer e.locker.Unlock()
+	if d.From == "" {
+		d.From = e.username
+	}
+	e.message.Reset()
+	e.message.SetHeader("From", d.From)
+	e.message.SetHeader("To", d.To)
+	e.message.SetHeader("Subject", d.Subject)
+	e.message.SetDateHeader("Date", time.Now())
+	e.message.SetBody("text/html", d.Msg)
+	return e.dialer.DialAndSend(e.message)
+}
--- test/main.go
+++ test/main.go
@@ -1,87 +1,28 @@
 package main
 
 import (
-	"encoding/binary"
-	"fmt"
-	"math"
-	"net"
-	"net/url"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
-	"github.com/xyzj/gopsu"
-	ginmiddleware "github.com/xyzj/gopsu/gin-middleware"
-	wmv2 "github.com/xyzj/wlstmicro/v2"
+	gomail "github.com/xyzj/gopsu/email"
 )
 
-var (
-	fw = wmv2.NewFrameWorkV2(`{"ver":"v1.0.0"}`)
-)
-
-func GetAvailablePort() (int, error) {
-	address, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:0", "0.0.0.0"))
-	if err != nil {
-		return 0, err
-	}
-	var listener *net.TCPListener
-	var found = false
-	for i := 0; i < 100; i++ {
-		listener, err = net.ListenTCP("tcp", address)
-		if err != nil {
-			continue
-		}
-		found = true
-	}
-	defer listener.Close()
-	if !found {
-		return 0, fmt.Errorf("could not find a useful port")
-	}
-	return listener.Addr().(*net.TCPAddr).Port, nil
-}
-func routesEngine() *gin.Engine {
-	// fw.LoadConfigure()
-	r := fw.NewHTTPEngine(ginmiddleware.ReadParams())
-	r.GET("/test", test)
-	return r
-}
-func test(c *gin.Context) {
-	println("RequestURI", c.Request.RequestURI)
-	println("uri requesturi", c.Request.URL.RequestURI())
-	println("rawquery", c.Request.URL.RawQuery, c.Request.URL.RawFragment, c.Request.URL.RawPath)
-	x, err := url.ParseQuery(c.Request.URL.RawQuery)
-	if err != nil {
-		println("err", err.Error())
-	}
-	println(len(x))
-	for k := range x {
-		println(k, x.Get(k))
-	}
-	c.String(200, "format string")
-}
-func String2Int8(s string, t int) byte {
-	x, _ := strconv.ParseUint(s, t, 8)
-	return byte(x)
-}
-func String2Int80(s string, t int) byte {
-	x, _ := strconv.ParseInt(s, t, 0)
-	return byte(x)
-}
-func Float64ToByte(float float64) []byte {
-	bits := math.Float64bits(float)
-	bytes := make([]byte, 8)
-	// binary.BigEndian.PutUint64(bytes, bits)
-	binary.LittleEndian.PutUint64(bytes, bits)
-
-	return bytes
-}
-
-// 启动文件 main.go
 func main() {
-	a := 12345678.90
-	b := gopsu.Float64ToByte(a)
-	println(gopsu.Bytes2String(b, "-"))
-	c := gopsu.Bytes2Float64(b, false)
-	println(fmt.Sprintf("%f", c))
-	bits := binary.LittleEndian.Uint64(b)
-	println(bits, gopsu.Bytes2Uint64(b, false))
+	m, err := gomail.NewEMail(&gomail.SMTPOpt{
+		Username: "minamoto.xu@hotmail.com",
+		Passwd:   "Someone@140821",
+		SMTPHost: "smtp.office365.com",
+	})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	err = m.Send(&gomail.Data{
+		To:      "xuyuan8720@189.cn",
+		Subject: "Gomail test subject",
+		Msg: `
+	GO 发送邮件，官方连包都帮我们写好了，真是贴心啊！！！`,
+	})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	println("email send")
 }
