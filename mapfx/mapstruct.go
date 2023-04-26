2076fa7e039ed67ec99abe18ea4de11472e9897c
unknow <beunknow@outlook.com>
2023-04-26 09:50:39 +0800

    upgrade mapfx

--- mapfx/mapstruct.go
+++ mapfx/mapstruct.go
@@ -11,7 +11,7 @@ import (
 // 	a string
 // }
 //
-// var z = NewStructMap[sample]()
+// var z = NewStructMap[string,sample]()
 // z.Store("a", &sample{
 // 	a: "132313",
 // })
@@ -22,48 +22,42 @@ import (
 type StructMapI interface{}
 
 // NewStructMap 返回一个线程安全的基于基本数据类型的map,key为string,value为StructMapI 类型的struct
-func NewStructMap[T StructMapI]() *StructMap[T] {
-	return &StructMap[T]{
+func NewStructMap[KEY int | int64 | uint64 | string, VALUE StructMapI]() *StructMap[KEY, VALUE] {
+	return &StructMap[KEY, VALUE]{
 		locker: sync.RWMutex{},
-		data:   make(map[string]*T),
+		data:   make(map[KEY]*VALUE),
 	}
 }
 
 // StructMap 泛型map 对应各种slice类型
-type StructMap[T StructMapI] struct {
+type StructMap[KEY int | int64 | uint64 | string, VALUE StructMapI] struct {
 	locker sync.RWMutex
-	data   map[string]*T
+	data   map[KEY]*VALUE
 }
 
 // Store 添加内容
-func (m *StructMap[T]) Store(key string, value *T) {
-	if key == "" {
-		return
-	}
+func (m *StructMap[KEY, VALUE]) Store(key KEY, value *VALUE) {
 	m.locker.Lock()
 	m.data[key] = value
 	m.locker.Unlock()
 }
 
 // Delete 删除内容
-func (m *StructMap[T]) Delete(key string) {
-	if key == "" {
-		return
-	}
+func (m *StructMap[KEY, VALUE]) Delete(key KEY) {
 	m.locker.Lock()
 	delete(m.data, key)
 	m.locker.Unlock()
 }
 
 // Clean 清空内容
-func (m *StructMap[T]) Clean() {
+func (m *StructMap[KEY, VALUE]) Clean() {
 	m.locker.Lock()
-	m.data = make(map[string]*T)
+	m.data = make(map[KEY]*VALUE)
 	m.locker.Unlock()
 }
 
 // Len 获取长度
-func (m *StructMap[T]) Len() int {
+func (m *StructMap[KEY, VALUE]) Len() int {
 	m.locker.RLock()
 	l := len(m.data)
 	m.locker.RUnlock()
@@ -73,7 +67,7 @@ func (m *StructMap[T]) Len() int {
 // Load 深拷贝一个值
 //
 //	获取的值可以安全编辑
-func (m *StructMap[T]) Load(key string) (*T, bool) {
+func (m *StructMap[KEY, VALUE]) Load(key KEY) (*VALUE, bool) {
 	m.locker.RLock()
 	v, ok := m.data[key]
 	m.locker.RUnlock()
@@ -87,7 +81,7 @@ func (m *StructMap[T]) Load(key string) (*T, bool) {
 // LoadForUpdate 浅拷贝一个值
 //
 //	可用于需要直接修改map内的值的场景，会引起map内值的变化
-func (m *StructMap[T]) LoadForUpdate(key string) (*T, bool) {
+func (m *StructMap[KEY, VALUE]) LoadForUpdate(key KEY) (*VALUE, bool) {
 	m.locker.RLock()
 	v, ok := m.data[key]
 	m.locker.RUnlock()
@@ -98,10 +92,7 @@ func (m *StructMap[T]) LoadForUpdate(key string) (*T, bool) {
 }
 
 // Has 判断Key是否存在
-func (m *StructMap[T]) Has(key string) bool {
-	if key == "" {
-		return false
-	}
+func (m *StructMap[KEY, VALUE]) Has(key KEY) bool {
 	m.locker.RLock()
 	defer m.locker.RUnlock()
 	if _, ok := m.data[key]; ok {
@@ -111,8 +102,8 @@ func (m *StructMap[T]) Has(key string) bool {
 }
 
 // Clone 深拷贝map,可安全编辑
-func (m *StructMap[T]) Clone() map[string]*T {
-	x := make(map[string]*T)
+func (m *StructMap[KEY, VALUE]) Clone() map[KEY]*VALUE {
+	x := make(map[KEY]*VALUE)
 	m.locker.RLock()
 	for k, v := range m.data {
 		z := *v
@@ -125,7 +116,7 @@ func (m *StructMap[T]) Clone() map[string]*T {
 // ForEach 遍历map的key和value
 //
 //	遍历前会进行深拷贝，可安全编辑
-func (m *StructMap[T]) ForEach(f func(key string, value *T) bool) {
+func (m *StructMap[KEY, VALUE]) ForEach(f func(key KEY, value *VALUE) bool) {
 	x := m.Clone()
 	defer func() {
 		if err := recover(); err != nil {
---
+++ mapfx/nonrepeating.go
@@ -0,0 +1,135 @@
+package mapfx
+
+import "sync"
+
+// 使用示例
+//
+// a := NewUniqueSlice[string]()
+// for _, v := range []string{"1", "2", "3", "3", "4", "5"} {
+// 	a.Store(v)
+// }
+// println(a.Slice()) // ["1", "2", "3", "4", "5"]
+//
+// for _, v := range []string{"1", "2", "3", "3", "4", "5"} {
+// 	if a.Store(v) { // 判断一下如果成功插入，说明原来没有值，可以进行下一步操作之类的
+// 		// do something
+// 	}
+// }
+
+// NewUniqueSlice 利用map构建一个内容不重复的slice
+//
+//	非线程安全，请勿跨线程使用
+func NewUniqueSlice[T byte | int8 | int | int32 | int64 | float32 | float64 | string]() *UniqueSlice[T] {
+	return &UniqueSlice[T]{
+		// locker: sync.RWMutex{},
+		data: make(map[T]struct{}),
+	}
+}
+
+// UniqueSlice 一个不重复的切片结构
+type UniqueSlice[T byte | int8 | int | int32 | int64 | float32 | float64 | string] struct {
+	// locker sync.RWMutex
+	data map[T]struct{}
+}
+
+func (u *UniqueSlice[T]) Store(item T) bool {
+	// u.locker.Lock()
+	// defer u.locker.Unlock()
+	if _, ok := u.data[item]; ok {
+		return false
+	}
+	u.data[item] = struct{}{}
+	return true
+}
+func (u *UniqueSlice[T]) StoreMany(items ...T) {
+	// u.locker.Lock()
+	// defer u.locker.Unlock()
+	for _, item := range items {
+		if _, ok := u.data[item]; ok {
+			continue
+		}
+		u.data[item] = struct{}{}
+	}
+}
+func (u *UniqueSlice[T]) Len() int {
+	// u.locker.RLock()
+	// defer u.locker.RUnlock()
+	return len(u.data)
+}
+func (u *UniqueSlice[T]) Slice() []T {
+	// u.locker.RLock()
+	// defer u.locker.RUnlock()
+	x := make([]T, 0, len(u.data))
+	for k := range u.data {
+		x = append(x, k)
+	}
+	return x
+}
+func (u *UniqueSlice[T]) Has(item T) bool {
+	for k := range u.data {
+		if k == item {
+			return true
+		}
+	}
+	return false
+}
+
+// NewUniqueSliceSafe 利用map构建一个内容不重复的slice
+//
+//	线程安全
+func NewUniqueSliceSafe[T byte | int8 | int | int32 | int64 | float32 | float64 | string]() *UniqueSlice[T] {
+	return &UniqueSlice[T]{
+		// locker: sync.RWMutex{},
+		data: make(map[T]struct{}),
+	}
+}
+
+// UniqueSliceSafe 一个不重复的切片结构
+type UniqueSliceSafe[T byte | int8 | int | int32 | int64 | float32 | float64 | string] struct {
+	locker sync.RWMutex
+	data   map[T]struct{}
+}
+
+func (u *UniqueSliceSafe[T]) Store(item T) bool {
+	u.locker.Lock()
+	defer u.locker.Unlock()
+	if _, ok := u.data[item]; ok {
+		return false
+	}
+	u.data[item] = struct{}{}
+	return true
+}
+func (u *UniqueSliceSafe[T]) StoreMany(items ...T) {
+	u.locker.Lock()
+	defer u.locker.Unlock()
+	for _, item := range items {
+		if _, ok := u.data[item]; ok {
+			continue
+		}
+		u.data[item] = struct{}{}
+	}
+}
+func (u *UniqueSliceSafe[T]) Len() int {
+	u.locker.RLock()
+	defer u.locker.RUnlock()
+	return len(u.data)
+}
+func (u *UniqueSliceSafe[T]) Slice() []T {
+	u.locker.RLock()
+	defer u.locker.RUnlock()
+	x := make([]T, 0, len(u.data))
+	for k := range u.data {
+		x = append(x, k)
+	}
+	return x
+}
+func (u *UniqueSliceSafe[T]) Has(item T) bool {
+	u.locker.RLock()
+	defer u.locker.RUnlock()
+	for k := range u.data {
+		if k == item {
+			return true
+		}
+	}
+	return false
+}
